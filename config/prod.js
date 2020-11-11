0111cf275ac91b2ecb2c4f5965ac84e4bf8c0739
Chandan Debnath <35869474+chandanadittyo@users.noreply.github.com>
2020-11-11 17:09:10 +0600

    first commit

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+//prod.js -  production key is here
+module.exports={
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClintSecret:process.env.GOOGLE_CLIENT_SECRET,
+    mongoURL: process.env.MONGO_URI,
+    cookieKey: process.env.COKKIE_KEY
+   
+
+}
